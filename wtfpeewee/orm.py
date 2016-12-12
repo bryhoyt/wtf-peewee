c6e6dbb3703a55ad7995ebe239dc8af978d711a9
bryhoyt <bryan@brush.co.nz>
2016-12-13 09:27:25 +1300

    Use ModelForm() in orm.py and factor the form creation logic into ModelConverter

--- wtfpeewee/orm.py
+++ wtfpeewee/orm.py
@@ -3,16 +3,21 @@ Tools for generating forms based on Peewee models
 (cribbed from wtforms.ext.django)
 """
 
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 from wtforms import Form
 from wtforms import fields as f
+from wtforms import widgets
 from wtforms import validators
+from wtfpeewee.fields import FormField
+from wtfpeewee.fields import HiddenIntegerField
 from wtfpeewee.fields import ModelSelectField
 from wtfpeewee.fields import SelectChoicesField
 from wtfpeewee.fields import SelectQueryField
 from wtfpeewee.fields import WPDateField
 from wtfpeewee.fields import WPDateTimeField
 from wtfpeewee.fields import WPTimeField
+from wtfpeewee.fields import ModelListField
+from wtfpeewee.forms import ModelForm
 from wtfpeewee._compat import text_type
 
 from peewee import BareField
@@ -32,10 +37,10 @@ from peewee import TextField
 from peewee import TimeField
 from peewee import TimestampField
 
-
 __all__ = (
     'FieldInfo',
     'ModelConverter',
+    'ModelForm',
     'model_fields',
     'model_form')
 
@@ -47,23 +52,23 @@ def handle_null_filter(data):
 FieldInfo = namedtuple('FieldInfo', ('name', 'field'))
 
 class ModelConverter(object):
-    defaults = {
-        BareField: f.TextField,
-        BigIntegerField: f.IntegerField,
-        BlobField: f.TextAreaField,
-        BooleanField: f.BooleanField,
-        CharField: f.TextField,
-        DateField: WPDateField,
-        DateTimeField: WPDateTimeField,
-        DecimalField: f.DecimalField,
-        DoubleField: f.FloatField,
-        FloatField: f.FloatField,
-        IntegerField: f.IntegerField,
-        PrimaryKeyField: f.HiddenField,
-        TextField: f.TextAreaField,
-        TimeField: WPTimeField,
-        TimestampField: WPDateTimeField,
-    }
+    defaults = OrderedDict((
+        (BareField, f.TextField),
+        (BigIntegerField, f.IntegerField),
+        (BlobField, f.TextAreaField),
+        (BooleanField, f.BooleanField),
+        (CharField, f.TextField),
+        (DateField, WPDateField),
+        (DateTimeField, WPDateTimeField),
+        (DecimalField, f.DecimalField),
+        (DoubleField, f.FloatField),
+        (FloatField, f.FloatField),
+        (PrimaryKeyField, HiddenIntegerField),     # TODO put through a separate PR just for bumping this up and using OrderedDict, as it's small and quite separate.
+        (IntegerField, f.IntegerField),
+        (TextField, f.TextAreaField),
+        (TimeField, WPTimeField),
+        (TimestampField, WPDateTimeField),
+    ))
     coerce_defaults = {
         BigIntegerField: int,
         CharField: text_type,
@@ -116,7 +121,7 @@ class ModelConverter(object):
         if field.null:
             # Treat empty string as None when converting.
             kwargs['filters'].append(handle_null_filter)
-
+            
         if (field.null or (field.default is not None)) and not field.choices:
             # If the field can be empty, or has a default value, do not require
             # it when submitting a form.
@@ -141,6 +146,7 @@ class ModelConverter(object):
                 return self.converters[converter](model, field, **kwargs)
         else:
             for converter in self.defaults:
+                print('IN DEFAULTS?', field.name, converter, field)
                 if not isinstance(field, converter):
                     # Early-continue because it simplifies reading the following code.
                     continue
@@ -166,39 +172,85 @@ class ModelConverter(object):
         raise AttributeError("There is not possible conversion "
                              "for '%s'" % type(field))
 
+    def convert_fields(self, model, allow_pk=False, only=None, exclude=None,
+                       field_args=None, include_inlines=False, depth=0):
+        """
+        Generate a dictionary of fields for a given Peewee model.
 
-def model_fields(model, allow_pk=False, only=None, exclude=None,
-                 field_args=None, converter=None):
+        See `model_form` docstring for description of parameters.
+        """
+        field_args = field_args or {}
+
+        model_fields = list(model._meta.sorted_fields)
+        inlines = OrderedDict((k, r) for k, r in sorted(model._meta.reverse_rel.items()))
+        if not allow_pk:
+            model_fields.pop(0)
+
+        if only:
+            model_fields = [x for x in model_fields if x.name in only]
+            inlines = OrderedDict((k, r) for k, r in inlines.items() if k in only)
+        elif exclude:
+            model_fields = [x for x in model_fields if x.name not in exclude]
+            inlines = OrderedDict((k, r) for k, r in inlines.items() if k not in exclude)
+
+        field_dict = {}
+        for model_field in model_fields:
+            name, field = self.convert(
+                model,
+                model_field,
+                field_args.get(model_field.name))
+            field_dict[name] = field
+
+        if include_inlines:
+            for name, foreign_key_field in inlines.items():
+                sub_only = None
+                sub_exclude = []
+                if only:
+                    sub_only = [n[len(name)+1:] for n in only if n.startswith(name+'.')]
+                if exclude:
+                    sub_exclude = [n[len(name)+1:] for n in exclude if n.startswith(name+'.')]
+                if foreign_key_field.name not in sub_exclude:
+                    sub_exclude.append(foreign_key_field.name)
+                inline_form = self.form(foreign_key_field.model_class, allow_pk=True,
+                                        only=sub_only, exclude=sub_exclude, depth=depth+1,
+                                        allow_delete=True, include_inlines=True,
+                                        field_args={foreign_key_field.model_class._meta.primary_key.name:
+                                                    {'validators': [validators.Optional()]}})
+
+
+
+                field_dict[name] = ModelListField(foreign_key_field,
+                                                  FormField(inline_form),
+                                                  depth=depth+1)
+
+        return field_dict
+    
+    def form(self, model, base_class=ModelForm, allow_pk=False, only=None, exclude=None,
+             field_args=None, allow_delete=False, include_inlines=False, depth=0):
+        """
+        Create a wtforms Form for a given Peewee model class::
+        
+        Parameters as per ``model_form``
+        """
+        field_dict = self.convert_fields(model, allow_pk=allow_pk, only=only,
+                                         exclude=exclude, field_args=field_args,
+                                         include_inlines=include_inlines, depth=depth)
+        form = type(model.__name__ + 'Form', (base_class, ), field_dict)
+        if allow_delete:
+            form.delete_ = f.BooleanField(default=False)
+        form.model = model
+        return form
+
+
+def model_fields(*args, **kwargs):
     """
-    Generate a dictionary of fields for a given Peewee model.
-
-    See `model_form` docstring for description of parameters.
+    Kept for backwards-compatibility: the code is now in ModelConvert.convert_fields().
     """
-    converter = converter or ModelConverter()
-    field_args = field_args or {}
-
-    model_fields = list(model._meta.sorted_fields)
-    if not allow_pk:
-        model_fields.pop(0)
-
-    if only:
-        model_fields = [x for x in model_fields if x.name in only]
-    elif exclude:
-        model_fields = [x for x in model_fields if x.name not in exclude]
-
-    field_dict = {}
-    for model_field in model_fields:
-        name, field = converter.convert(
-            model,
-            model_field,
-            field_args.get(model_field.name))
-        field_dict[name] = field
-
-    return field_dict
+    converter = kwargs.pop('converter', None) or ModelConverter()
+    return converter.convert_fields(*args, **kwargs)
 
 
-def model_form(model, base_class=Form, allow_pk=False, only=None, exclude=None,
-               field_args=None, converter=None):
+def model_form(*args, **kwargs):
     """
     Create a wtforms Form for a given Peewee model class::
 
@@ -222,6 +274,8 @@ def model_form(model, base_class=Form, allow_pk=False, only=None, exclude=None,
     :param converter:
         A converter to generate the fields based on the model properties. If
         not set, ``ModelConverter`` is used.
+    :param include_inlines
+        Generate inline models (from ForeignKeyFields that point to this model)
     """
-    field_dict = model_fields(model, allow_pk, only, exclude, field_args, converter)
-    return type(model.__name__ + 'Form', (base_class, ), field_dict)
+    converter = kwargs.pop('converter', None) or ModelConverter()
+    return (converter or ModelConverter()).form(*args, **kwargs)
